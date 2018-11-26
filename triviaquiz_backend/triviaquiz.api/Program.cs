ad35fc48ffc1f09396807046e61234fbfe3aa019
jda <jda@it-minds.dk>
2018-11-26 13:16:48 +0100

    Removed MVC project and added API project instead

--- triviaquiz_backend/triviaquiz.api/Program.cs
+++ triviaquiz_backend/triviaquiz.api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace triviaquiz_api
+namespace triviaquiz.api
 {
     public class Program
     {
