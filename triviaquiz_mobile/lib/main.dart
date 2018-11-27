ee1a7bfb4196312560b417012978cd7bb1a7bb8b
Rasmus Thorsen <rot@it-minds.dk>
2018-11-27 13:09:42 +0100

    category tests

--- triviaquiz_mobile/lib/main.dart
+++ triviaquiz_mobile/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:triviaquiz_mobile/src/app.dart';
 
-void main() => runApp(App());
+void main() {
+  runApp(App());
+}
--- triviaquiz_mobile/lib/src/resources/category_api_provider.dart
+++ triviaquiz_mobile/lib/src/resources/category_api_provider.dart
@@ -6,12 +6,12 @@ import 'package:triviaquiz_mobile/src/models/category_model.dart';
 
 class CategoryApiProvider {
   Client client = Client();
-  final String apiEndPoint = 'http://localhost:44309/api';
-  
+  final String apiEndPoint = 'https://10.0.2.2:5001/api';
+
   Future<List<CategoryModel>> fetchCategories() async {
     final response = await client.get('$apiEndPoint/lobby/categories');
     final categoriesJson = json.decode(response.body);
-
+    print(response);
     return categoriesJson.cast<CategoryModel>();
   }
 }
